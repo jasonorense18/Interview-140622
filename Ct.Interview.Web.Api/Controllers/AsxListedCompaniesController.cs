bd1823c2c90f1f65f2d268b0b0508b359b570179
Aurelien <aure303@gmail.com>
2022-06-14 11:10:34 +1000

    Init

---
+++ Ct.Interview.Web.Api/Controllers/AsxListedCompaniesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ct.Interview.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsxListedCompaniesController : ControllerBase
+    {
+        private readonly IAsxListedCompaniesService _asxListedCompaniesService;
+
+        public AsxListedCompaniesController(IAsxListedCompaniesService asxListedCompaniesService)
+        {
+            _asxListedCompaniesService = asxListedCompaniesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<AsxListedCompanyResponse[]>> Get(string asxCode)
+        {
+            var asxListedCompanies = await _asxListedCompaniesService.GetByAsxCode(asxCode);
+
+            return Ok(asxListedCompanies);
+        }
+    }
+}
